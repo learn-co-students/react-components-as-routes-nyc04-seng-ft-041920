6c88b749ae105c73650bc0791dcc850db2a0a470
senadakadric <skadric@fordham.edu>
2020-07-22 19:07:25 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import About from "./About";
+import Login from "./Login";
+import Home from "./Home";
+import Navbar from "./Navbar";
 
 ReactDOM.render(
-  <Home />,
-  document.getElementById('root')
+  // Router is the alias for BrowserRouter component, which is where we declare how React Router will be used
+  <Router>
+    <div>
+      <Navbar />
+      {/* In the Route component we say "when the URL matches this specified path, render this specified component" */}
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/login" component={Login} />
+      {/* <Route path="/senada" render={() => <h1>Senada</h1>} /> */}
+    </div>
+  </Router>,
+  document.getElementById("root")
 );
