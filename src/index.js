91114b0f53964f45e1a801121150a1e44a32b80f
Gamoundo <akilgg@gmail.com>
2020-08-02 13:27:52 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Home from './Home'
+import About from './About'
+import Login from './Login'
+import Navbar from './Navbar'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
+ReactDOM.render((
+//  make a router element with a route component, the props for 
+//  the route component are path="/component name" and 
+//  component={component name}
+// Wrap inside a div so you can have multiple routes
 
-ReactDOM.render(
-  <Home />,
+
+
+
+ <Router>
+   <div>
+     <Navbar />
+   <Route exact path="/" component={Home} />
+   <Route exact path="/about" component={About} />
+   <Route exact path="/login" component={Login} />
+   </div>
+  </Router>),
   document.getElementById('root')
 );
